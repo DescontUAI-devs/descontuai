eef2b045e08daa1e41682380b9a111c55268fe6d
Leonardo Flores <leonardocouy@hotmail.com>
2017-03-21 18:04:30 -0300

    Formulário de Contato integrado no email

--- app.py
+++ app.py
@@ -2,11 +2,22 @@
 from datetime import datetime
 from flask import Flask
 from flask import render_template
+from flask_mail import Mail, Message
 from flask_restful import Resource, Api, reqparse
-
+from decouple import config
 app = Flask(__name__)
 api = Api(app)
 
+# EMAIL CONFIGURATION
+
+app.config['MAIL_SERVER'] = config('MAIL_SERVER')
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = config('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = config('MAIL_PASSWORD')
+
+mail = Mail(app)
 
 class Customer(Resource):
     def post(self):
@@ -21,16 +32,35 @@ class Customer(Resource):
         return content, content['status_code']
 
 
+class Contact(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, location='json')
+        parser.add_argument('email', type=str, required=True, location='json')
+        parser.add_argument('phone', type=str, location='json')
+        parser.add_argument('description', type=str, required=True, location='json')
+
+        data = parser.parse_args(strict=True)
+        data['enviado_em'] = datetime.now()
+
+        # Email Message
+
+        msg = Message(
+            '[Descontuai] Dúvida de {}'.format(data.name),
+            sender='doscontuai@gmail.com',
+            recipients=
+            ['doscontuai@gmail.com'])
+
+        msg.html = render_template('contact_mail_template.html', contact=data)
+
+        mail.send(msg)
+
+        content = {"mensagem": "Dúvida enviada com sucesso!", "status_code": 200}
+
+        return content, content['status_code']
+
 api.add_resource(Customer, '/api/v1/customer')
-
-# Todo: Connect app with MongoDB Server using Flask-Pymongo
-
-# Todo: Parse parameters passed in API using reqparse (RequestParser)
-
-# Todo: create API for register a new user (customer). (Only POST method) using Flask-RESTFul
-
-# Todo: Implement Payment Gateway (Pagseguro).
-
+api.add_resource(Contact, '/api/v1/contact')
 
 @app.route("/")
 def index():
