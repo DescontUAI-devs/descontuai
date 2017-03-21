bca6db931eee6dd7c641037cdf007d8ab27e58a6
Carol Faria <kah.faria@hotmail.com>
2017-03-21 00:48:56 -0300

    Adiciona API de cadastro de usuario

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 # Configuring App
+from datetime import datetime
 from flask import Flask
 from flask import render_template
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
+
+class Customer(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, location='json')
+        parser.add_argument('email', type=str, required=True, location='json')
+
+        data = parser.parse_args(strict=True)
+        data['enviado_em'] = datetime.now()
+        content = {"mensagem": "Cliente cadastrado com sucesso!", "status_code": 200}
+
+        return content, content['status_code']
+
+
+api.add_resource(Customer, '/api/v1/customer')
 
 # Todo: Connect app with MongoDB Server using Flask-Pymongo
 
